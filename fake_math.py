e1acad07292a0cc86b49af9a66ac2a8bf1682c22
Poleshook <poleshook@mail.ru>
2024-08-25 12:48:05 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if second == 0:
+        print("Ошибка. Делить на ноль нельзя")
+    else:
+        print(first / second)
---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+from fake_math import divide as fd
+from true_math import divide as td
+
+
+fd(69, 3)
+fd(3, 0)
+td(49, 7)
+td(15, 0)
